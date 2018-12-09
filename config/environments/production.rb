b6c4587474399c402d7995930db0d2aeb5f75214
Sulav Adhikari <adhikari485@gmail.com>
2018-12-09 18:49:08 +0000

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'http://force-request-2018.herokuapp.com'}#for heroku
+  config.action_mailer.default_url_options = { host: 'http://sample-force-request.herokuapp.com.herokuapp.com'}#for heroku
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
