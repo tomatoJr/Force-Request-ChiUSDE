722ad0b94ccda1502beb033f897ae153c2f9465b
dong <dongineaters@gmail.com>
2019-11-06 21:54:09 +0000

    course fix

--- app/controllers/student_requests_controller.rb
+++ app/controllers/student_requests_controller.rb
@@ -561,8 +561,8 @@ class StudentRequestsController < ApplicationController
     
     #------------- fall 2019 --------------
     @requestCourse = StudentRequest::COURSE_LIST
-    @requestSession_629 = StudentRequest::SESSION_LIST_629
-    @requestSession_606 = StudentRequest::SESSION_LIST_606
+    # @requestSession_629 = StudentRequest::SESSION_LIST_629
+    @requestSession = StudentRequest::SESSION_LIST
     #------------- fall 2019 --------------
     
     @majorList = Major.pluck(:major_id)
--- app/models/student_request.rb
+++ app/models/student_request.rb
@@ -47,8 +47,8 @@ class StudentRequest < ActiveRecord::Base
     
     # ------------- fall 2019 --------------
     COURSE_LIST = [StudentRequest::COURSE_KEY_1,COURSE_KEY_2]
-    SESSION_LIST_629 = [SESSION_ID_629_1,SESSION_ID_629_2]
-    SESSION_LIST_606 = [StudentRequest::SESSION_ID_606_1]
+    # SESSION_LIST_629 = [SESSION_ID_629_1,SESSION_ID_629_2]
+    SESSION_LIST = [StudentRequest::SESSION_ID_606_1]
     #------------- fall 2019 --------------
     
     #Classification
